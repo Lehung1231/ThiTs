ed7e968fcd688c2de6457e1d2d756d3cc9b9f41c
Lehung1231 <hungltph22978@fpt.edu.vn>
2023-04-12 19:16:11 +0700

    updaate

--- THITS/src/App.tsx
+++ THITS/src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Router  } from "react-router-dom";
 import './App.css'
+import AdminProject from "./components/admin/adminProject";
+import AdminLayout from "./components/admin/adminLayout";
+import AdminEdit from "./components/admin/editAdmin";
+import AdminAdd from "./components/admin/addAdmin";
+import Signup from "./components/admin/singup";
+import Signin from "./components/admin/singin";
+import User from "./components/admin/user";
 
 function App() {
  
 
-  return (
-     <p className='text-red-400'>OKOKAD</p>
-  )
+  return <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<AdminLayout/>}>
+        <Route index element={<AdminProject></AdminProject>}/>
+        <Route path='/edit/:id' element={<AdminEdit/>}/>
+        <Route path='/add' element={<AdminAdd/>}/>
+        <Route path='/singup' element={<Signup/>}/>
+        <Route path='/singin' element={<Signin/>}/>
+      </Route>
+      <Route path='/user' element={<User/>}>
+      </Route>
+      </Routes>
+   </BrowserRouter>
+     
+  
 }
 
 export default App
